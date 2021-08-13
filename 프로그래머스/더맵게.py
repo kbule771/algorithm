6a4aa32698c85ef55af76c3d9e708aecacaa53f4
조완석 <wansuk771@gmail.com>
2021-08-13 21:43:32 +0900

    프로그래머스 더 맵게

--- 프로그래머스/더맵게.py
+++ 프로그래머스/더맵게.py
@@ -1,8 +1,22 @@
 import heapq
-
-li = [4,3,1,5]
-
-heap = heapq.heapify(li)
-for i in range(4):
-    a = heapq.heappop(li)
-    print(a)
+def solution(scoville, K):
+    answer = 0
+    heap = []
+    for sco in scoville:
+        heapq.heappush(heap,sco)
+    cnt = 0
+    while True:
+        cnt += 1
+        if heap:
+            a = heapq.heappop(heap)
+            if a >= K:
+                cnt -= 1
+                answer = cnt
+                break
+        if heap:
+            b = heapq.heappop(heap)
+            heapq.heappush(heap,a+2*b)
+        else:
+            answer = -1
+            break
+    return answer
